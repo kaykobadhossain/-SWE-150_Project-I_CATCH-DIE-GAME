8091911468a6c6edde0b8fdf4412ec782b34407f
KAYKOBAD HOSSAIN TANJIL <kaykobad36@student.sust.edu>
2023-02-23 01:12:17 +0600

    how mouse and keyboard works

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
 using namespace sf;
+using namespace std;
 
 int main()
 {
@@ -16,6 +18,16 @@ int main()
 
             if (event.type == Event::Closed)
                 app.close();
+
+            if(event.type==Event::KeyPressed)
+                cout<<"keyboard has been pressed"<<endl;
+
+            if(event.type==Event::MouseButtonPressed)
+                cout<<"Mouse has been pressed"<<endl;
+                if (event.key.code == Mouse::Left)
+                    cout<<"Left mouse pressed"<<endl;
+                else if (event.key.code == Mouse::Right)
+                    cout<<"Right mouse pressed"<<endl;
         }
 
 
